aa14dca386d4503a7e4fb864b8663ebbe52b3fa6
kasun <88395585+ka0un@users.noreply.github.com>
2025-01-19 17:59:22 +0530

    init

---
+++ src/main/java/com/hapangama/premierevents/PremierEventsApplication.java
@@ -0,0 +1,13 @@
+package com.hapangama.premierevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PremierEventsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hapangama/premierevents/PremierEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapangama.premierevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
